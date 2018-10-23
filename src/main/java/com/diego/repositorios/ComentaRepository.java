30980667003e9574ecbf25864565b8db51112aa4
DiegoGutierrezFing <43502194+DiegoGutierrezFing@users.noreply.github.com>
2018-10-23 19:11:32 -0300

    Update ComentaRepository.java

--- src/main/java/com/diego/repositorios/ComentaRepository.java
+++ src/main/java/com/diego/repositorios/ComentaRepository.java
@@ -5,7 +5,7 @@ package com.diego.repositorios;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.chronicler.model.Comenta;
+import com.diego.model.Comenta;
 
 @RepositoryRestResource(collectionResourceRel = "comentas", path = "comentas")
 public interface ComentaRepository extends PagingAndSortingRepository<Comenta, Long> {
