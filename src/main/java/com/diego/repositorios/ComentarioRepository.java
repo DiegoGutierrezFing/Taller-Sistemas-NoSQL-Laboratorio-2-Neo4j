6099887c920304e2dd4869ddaf9580f7f5e17459
DiegoGutierrezFing <43502194+DiegoGutierrezFing@users.noreply.github.com>
2018-10-23 19:11:51 -0300

    Update ComentarioRepository.java

--- src/main/java/com/diego/repositorios/ComentarioRepository.java
+++ src/main/java/com/diego/repositorios/ComentarioRepository.java
@@ -4,7 +4,7 @@ package com.diego.repositorios;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.chronicler.model.Comentario;
+import com.diego.model.Comentario;
 
 @RepositoryRestResource(collectionResourceRel = "comentarios", path = "comentarios")
 public interface ComentarioRepository extends PagingAndSortingRepository<Comentario, Long> {}
